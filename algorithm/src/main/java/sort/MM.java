5538b48f2a738156a7fa465ef180fe775927a863
wangpf <wangpf@sqbj.com>
2022-03-18 06:24:56 +0800

    n 阶矩阵的逆置运算

--- algorithm/src/main/java/sort/MM.java
+++ algorithm/src/main/java/sort/MM.java
@@ -1,2 +1,21 @@
-package sort;public class MM {
+package sort;
+
+/**
+ * n 阶矩阵的逆置运算
+ */
+public class MM {
+
+    int n;
+
+    void MM(int A[][]){
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = A[i][j];
+                A[i][j] = A[j][i];
+                A[j][i] = temp;
+            }
+        }
+    }
+
 }
