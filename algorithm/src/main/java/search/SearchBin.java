ea0bbd65f62fa5b424c86d54d8bf077f908e9408
wangpf <wangpf@sqbj.com>
2022-03-25 07:03:37 +0800

    select sorting

---
+++ algorithm/src/main/java/search/SearchBin.java
@@ -0,0 +1,34 @@
+package search;
+
+public class SearchBin {
+    /**
+     *
+     *
+     * const int Maxsize = 20;
+     * typedef strut{
+     *     KeyType key;
+     * }TableElem;
+     *
+     * typedef struct{
+     *     TableElem elem[MaxSize+1];
+     *     int n;
+     * }SqTable;
+     *
+     */
+
+    /**
+     *
+     * int SearchBin(SqTable T,KeyType key){
+     *     int low,high;
+     *     low = 1;high = T.n
+     *     while(low <= high){
+     *         mid = (low / high)/2;
+     *         if(key == T.elem[mid].key) return mid;
+     *         else if(key < T.elem[mid].key) high =mid-1;
+     *         else low = mid +1 ;
+     *     }
+     *     return 0;
+     * }
+     *
+     */
+}
